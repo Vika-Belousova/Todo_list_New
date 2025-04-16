1184444cbfcec35260b68a8a7bc546293df375a0
Vika-Belousova <91773388+Vika-Belousova@users.noreply.github.com>
2025-04-16 18:17:05 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import TaskBoardPresenter from './presenter/tasks-board-presenter.js';
 import TaskModel from './model/task-model.js';
 
+import HeaderComponent from './view/header-component.js';
+import FormAddTaskComponent from './view/form-add-task-component.js';
+import { render, RenderPosition } from './framework/render.js';
+
 const bodyContainer = document.querySelector('.container');
 const taskBoardContainer = document.querySelector('.container_block');
 
@@ -10,5 +14,15 @@ const taskBoardPresenter = new TaskBoardPresenter({
   boardContainer: taskBoardContainer,
   taskModel,
 });
+
+
+const headerComponent = new HeaderComponent();
+render(headerComponent, bodyContainer, RenderPosition.BEFOREBEGIN);
+
+
+const formComponent = new FormAddTaskComponent();
+render(formComponent, bodyContainer, RenderPosition.AFTERBEGIN);
+
+
 
 taskBoardPresenter.init();
