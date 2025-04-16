a1923bb74c689616ff53f36746ce1ba51c8cb144
Vika-Belousova <91773388+Vika-Belousova@users.noreply.github.com>
2025-04-16 18:16:40 +0300

    Update form-add-task-component.js

--- src/view/form-add-task-component.js
+++ src/view/form-add-task-component.js
@@ -1,4 +1,3 @@
-// src/view/form-add-task-component.js
 import { AbstractComponent } from '../framework/view/abstract-component.js';
 
 function createFormAddTaskComponentTemplate() {
@@ -12,31 +11,7 @@ function createFormAddTaskComponentTemplate() {
 }
 
 export default class FormAddTaskComponent extends AbstractComponent {
-  #onAddTask;
-
-  constructor(onAddTask) {
-    super();
-    this.#onAddTask = onAddTask;
-  }
-
   get template() {
     return createFormAddTaskComponentTemplate();
-  }
-
-  get element() {
-    const element = super.element;
-
-    const inputElement = element.querySelector('input');
-    const buttonElement = element.querySelector('button');
-
-    buttonElement.addEventListener('click', () => {
-      const taskName = inputElement.value.trim();
-      if (taskName) {
-        this.#onAddTask(taskName);
-        inputElement.value = '';
-      }
-    });
-
-    return element;
   }
 }
