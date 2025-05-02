1fe0d6b9dda7033c8c1b5c1bdcb2b28534036ace
Vika-Belousova <91773388+Vika-Belousova@users.noreply.github.com>
2025-05-02 22:52:47 +0300

    Update clear-button-component.js

--- src/view/clear-button-component.js
+++ src/view/clear-button-component.js
@@ -1,26 +1,40 @@
 import { AbstractComponent } from '../framework/view/abstract-component.js';
 
-function createClearButtonTemplate() {
-  return `<button class="clear-button">Очистить корзину</button>`;
+function createClearButtonTemplate(isDisabled) {
+  return `<button class="clear-button  ${isDisabled ? 'clear-button--disabled' : ''}">Очистить корзину</button>`;
 }
 
 export default class ClearButtonComponent extends AbstractComponent {
   #onClearCart = null;
+  #isDisabled = false;
 
-  constructor(onClear) {
+  constructor(onClear, isDisabled = false) {
     super();
     this.#onClearCart = onClear;
+    this.#isDisabled = isDisabled;
     this.#setClickHandler();
   }
 
   get template() {
-    return createClearButtonTemplate();
+    return createClearButtonTemplate(this.#isDisabled);
   }
 
   #setClickHandler() {
     this.element.addEventListener('click', (evt) => {
+      if (this.#isDisabled) return;
+      
       evt.preventDefault();
       this.#onClearCart();
     });
+  }
+
+  disable() {
+    this.#isDisabled = true;
+    this.element.classList.add('clear-button--disabled');
+  }
+
+  enable() {
+    this.#isDisabled = false;
+    this.element.classList.remove('clear-button--disabled');
   }
 }
